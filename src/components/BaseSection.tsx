8e2d25216cc1d0b81fa2304ce0a7c0e77fc9e59c
Alejandro Coca <alxmcr@outlook.com>
2024-05-10 14:03:18 -0400

    wip

--- src/components/BaseSection.tsx
+++ src/components/BaseSection.tsx
@@ -5,13 +5,17 @@ type Props = {
 };
 
 export default function BaseSection({ children, id = '', isWidthFull = false }: Props) {
+  if (isWidthFull) {
+    return (
+      <section id={id} className="flex min-h-screen justify-center">
+        <div className="w-full">{children}</div>
+      </section>
+    );
+  }
+
   return (
     <section id={id} className="flex min-h-screen justify-center px-[16px] md:px-[40px] lg:px-0">
-      {isWidthFull ? (
-        <div className="w-full">{children}</div>
-      ) : (
-        <div className="w-full lg:w-[1028px]">{children}</div>
-      )}
+      <div className="w-full lg:w-[1028px]">{children}</div>
     </section>
   );
 }
