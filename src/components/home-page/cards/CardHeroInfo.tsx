a2400e743c3c8c51c319a80aac547fe22c0b67d8
Alejandro Coca <alxmcr@outlook.com>
2024-05-10 17:53:02 -0400

    wip

--- src/components/home-page/cards/CardHeroInfo.tsx
+++ src/components/home-page/cards/CardHeroInfo.tsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 export default function CardHeroInfo() {
   return (
-    <article className="font-groteskspace flex flex-col gap-12 border border-golden-300 md:w-[478px]">
+    <article className=" flex flex-col gap-12 font-groteskspace md:w-[478px]">
       <div className="flex flex-col justify-center gap-7">
         <h1 className="font-rowdies text-[40px] font-bold text-golden-300 md:text-[48px]">Muvifav</h1>
         <p className="text-[24px] text-light-50 md:text-[28px]">
