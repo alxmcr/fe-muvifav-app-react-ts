9ec254f8d0df9a044526c5c75c7b17816d9143a4
Alejandro Coca <alxmcr@outlook.com>
2024-05-10 14:34:37 -0400

    wip

--- src/components/home-page/sections/HeroSection.tsx
+++ src/components/home-page/sections/HeroSection.tsx
@@ -3,7 +3,7 @@ import BaseSection from '../../BaseSection';
 export default function HeroSection() {
   return (
     <BaseSection id="hero" isWidthFull>
-      <div className="flex h-full flex-col items-center justify-center gap-4 bg-[url('/assets/backgrounds/bg-hero.jpg')] bg-auto bg-center bg-no-repeat px-2 py-3 md:h-[500px]">
+      <div className="flex h-full flex-col items-center justify-center gap-4 bg-[url('/assets/backgrounds/bg-hero.png')] bg-auto bg-center bg-no-repeat px-2 py-3 md:h-[500px]">
         <h1>Hola!</h1>
       </div>
     </BaseSection>
