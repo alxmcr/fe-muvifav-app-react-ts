bb28c505123da869aeaa2e3b709dd80f9dbec6b8
Alejandro Coca <alxmcr@outlook.com>
2024-05-10 00:41:12 -0400

    tailwind color

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ export default function HomePage() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="size-[40px] bg-daisy-600"></div>
     </main>
   );
 }
