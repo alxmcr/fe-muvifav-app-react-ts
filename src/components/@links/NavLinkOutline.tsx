1eda3b93b2b3cc7f918a5e2d3858e0312ce92d56
Alejandro Coca <alxmcr@outlook.com>
2024-05-11 17:31:34 -0400

    wip

--- src/components/@links/NavLinkOutline.tsx
+++ src/components/@links/NavLinkOutline.tsx
@@ -9,7 +9,7 @@ export default function NavLinkOutline({ navLink }: Props) {
   return (
     <NavLink
       to={navLink.href}
-      className="flex h-12 w-[130px] items-center justify-center rounded-lg border border-golden-300 font-groteskspace hover:bg-golden-300 hover:text-light-950"
+      className="flex h-12 w-[130px] items-center justify-center rounded-lg border border-golden-300 font-groteskspace text-golden-300 hover:bg-golden-300 hover:text-light-950"
     >
       {navLink.text}
     </NavLink>
--- src/components/navigation/AppHeader.tsx
+++ src/components/navigation/AppHeader.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 export default function AppHeader({ navLinks = [] }: Props) {
   return (
-    <header className="flex justify-center border border-golden-300">
-      <div className="flex w-full items-center justify-between border border-pink-400 px-4 md:px-10 lg:w-[1028px] lg:px-0">
+    <header className="absolute flex w-full justify-center border-4 border-golden-300 py-4">
+      <div className="mx-auto flex w-full items-center justify-between border-4 border-pink-400 px-4 md:px-10 lg:w-[1028px] lg:px-0">
         <LogoMuvifav />
         <AppNavigation navLinks={navLinks} />
       </div>
