9ef5f72c2810ecc739f1a781a534d822605306b0
Alejandro Coca <alxmcr@outlook.com>
2024-05-11 00:53:38 -0400

    wip

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import Icon44x44ArrowLeft from '../components/@icons/44x44/Icon44x44ArrowLeft';
 import Icon44x44ArrowRight from '../components/@icons/44x44/Icon44x44ArrowRight';
+import LogoMuvifav from '../components/@logos/48x48/LogoMuvifav';
 import BaseSection from '../components/BaseSection';
 import HeroSection from '../components/home-page/sections/HeroSection';
 import useMovie from '../hooks/useMovie';
@@ -15,6 +16,7 @@ export default function HomePage() {
       <BaseSection>
         <h1>Holas!</h1>
         <p>
+          <LogoMuvifav />
           <Icon44x44ArrowLeft />
           <Icon44x44ArrowRight />
         </p>
