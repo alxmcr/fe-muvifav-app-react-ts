62aa6916f82e665ae098e4998ec99b77ce820cfc
Alejandro Coca <alxmcr@outlook.com>
2024-05-11 14:33:35 -0400

    wip

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,9 @@ import Icon44x44ArrowRight from '../components/@icons/44x44/Icon44x44ArrowRight'
 import LogoMuvifav from '../components/@logos/48x48/LogoMuvifav';
 import BaseSection from '../components/BaseSection';
 import HeroSection from '../components/home-page/sections/HeroSection';
+import AppHeader from '../components/navigation/AppHeader';
 import useMovie from '../hooks/useMovie';
+import { MOCK_NAV_LINKS } from '../mocks/mock-nav-links';
 
 export default function HomePage() {
   const idMovie = 748783;
@@ -12,6 +14,7 @@ export default function HomePage() {
 
   return (
     <main>
+      <AppHeader navLinks={MOCK_NAV_LINKS} />
       <HeroSection />
       <BaseSection>
         <h1>Holas!</h1>
