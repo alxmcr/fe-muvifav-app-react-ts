033fe8645e9da524e6f5255a07b846d6a430870d
Alejandro Coca <alxmcr@outlook.com>
2024-05-10 22:59:44 -0400

    wip

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,9 @@
 import BaseSection from '../components/BaseSection';
 import HeroSection from '../components/home-page/sections/HeroSection';
-import usePopularMovies from '../hooks/usePopularMovies';
+import useTopRatedMovies from '../hooks/useTopRatedMovies';
 
 export default function HomePage() {
-  const results = usePopularMovies(1);
+  const results = useTopRatedMovies(1);
   console.log('🚀 ~ HomePage ~ results:', results);
 
   return (
--- src/services/TMDB/ITMDBService.ts
+++ src/services/TMDB/ITMDBService.ts
@@ -3,4 +3,5 @@ import { ITMDBFindAllResponse } from '../../@types/serviceTypes';
 export interface ITMDBService {
   findAllPopularMoviesByPage(pageNumber: number): Promise<ITMDBFindAllResponse>;
   findAllTopRatedMoviesByPage(pageNumber: number): Promise<ITMDBFindAllResponse>;
+  findAllUpcomingMoviesByPage(pageNumber: number): Promise<ITMDBFindAllResponse>;
 }
