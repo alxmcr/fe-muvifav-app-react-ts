8b0832e76361b8a66783b992f1e3f6ccfdb42955
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2024-05-11 00:54:06 -0400

    Chore/icons (#3)

---
+++ src/components/@logos/48x48/LogoMuvifav.tsx
@@ -0,0 +1,11 @@
+export default function LogoMuvifav() {
+  return (
+    <svg width={48} height={48} viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width={48} height={48} rx={8} fill="#FFDD66" />
+      <path
+        d="M34.5 9V12.3333H31V9H17V12.3333H13.5V9H10V39H13.5V35.6667H17V39H31V35.6667H34.5V39H38V9H34.5ZM17 32.3333H13.5V29H17V32.3333ZM17 25.6667H13.5V22.3333H17V25.6667ZM17 19H13.5V15.6667H17V19ZM34.5 32.3333H31V29H34.5V32.3333ZM34.5 25.6667H31V22.3333H34.5V25.6667ZM34.5 19H31V15.6667H34.5V19Z"
+        fill="black"
+      />
+    </svg>
+  );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,6 @@
+import Icon44x44ArrowLeft from '../components/@icons/44x44/Icon44x44ArrowLeft';
+import Icon44x44ArrowRight from '../components/@icons/44x44/Icon44x44ArrowRight';
+import LogoMuvifav from '../components/@logos/48x48/LogoMuvifav';
 import BaseSection from '../components/BaseSection';
 import HeroSection from '../components/home-page/sections/HeroSection';
 import useMovie from '../hooks/useMovie';
@@ -5,13 +8,18 @@ import useMovie from '../hooks/useMovie';
 export default function HomePage() {
   const idMovie = 748783;
   const movie = useMovie(idMovie);
-  console.log("🚀 ~ HomePage ~ movie:", movie)
+  console.log('🚀 ~ HomePage ~ movie:', movie);
 
   return (
     <main>
       <HeroSection />
       <BaseSection>
         <h1>Holas!</h1>
+        <p>
+          <LogoMuvifav />
+          <Icon44x44ArrowLeft />
+          <Icon44x44ArrowRight />
+        </p>
       </BaseSection>
     </main>
   );
