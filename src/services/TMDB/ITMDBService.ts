49328f0eb879091992d21b7a107f5b04a0e7ea3f
Alejandro Coca <alxmcr@outlook.com>
2024-05-10 20:11:58 -0400

    wip

--- src/services/TMDB/ITMDBService.ts
+++ src/services/TMDB/ITMDBService.ts
@@ -2,4 +2,5 @@ import { ITMDBFindAllResponse } from '../../@types/serviceTypes';
 
 export interface ITMDBService {
   findAllPopularMoviesByPage(pageNumber: number): Promise<ITMDBFindAllResponse>;
+  findAllTopRatedMoviesByPage(pageNumber: number): Promise<ITMDBFindAllResponse>;
 }
--- src/services/TMDB/TMDBService.ts
+++ src/services/TMDB/TMDBService.ts
@@ -9,8 +9,9 @@ export class TMDBService implements ITMDBService {
     this.baseUrl = `${import.meta.env.VITE_TMDB_API_BASE_URL}/${import.meta.env.VITE_TMDB_API_VERSION}`;
     this.apiAccessToken = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
   }
+  async findAllTopRatedMoviesByPage(pageNumber = 1): Promise<ITMDBFindAllResponse> {
+    const endpoint = `movie/top_rated`;
 
-  async findAllPopularMoviesByPage(pageNumber = 1): Promise<ITMDBFindAllResponse> {
     const options = {
       method: 'GET',
       headers: {
@@ -19,7 +20,28 @@ export class TMDBService implements ITMDBService {
       },
     };
 
+    const queryParams = `language=en-US&page=${pageNumber}&sort_by=created_at.asc`;
+
+    const response: Response = await fetch(`${this.baseUrl}/${endpoint}?${queryParams}`, options);
+
+    if (response.status === 200) {
+      return response.json();
+    }
+
+    return Promise.reject(response);
+  }
+
+  async findAllPopularMoviesByPage(pageNumber = 1): Promise<ITMDBFindAllResponse> {
     const endpoint = `movie/popular`;
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${this.apiAccessToken}`,
+      },
+    };
+
     const queryParams = `language=en-US&page=${pageNumber}&sort_by=created_at.asc`;
 
     const response: Response = await fetch(`${this.baseUrl}/${endpoint}?${queryParams}`, options);
