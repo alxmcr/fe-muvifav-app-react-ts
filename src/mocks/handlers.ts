73a7cf5c7b90503275f38849687872930c54c8b7
Alejandro Coca <alxmcr@outlook.com>
2024-05-12 01:10:21 -0400

    wip

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,8 +2,18 @@ import { http, HttpResponse } from 'msw';
 import mockPopularMovies from './data/mock-popular-movies.json';
 import mockTopRatedMovies from './data/mock-top-rated-movies.json';
 import mockUpcomingMovies from './data/mock-upcoming-movies.json';
+import mockMovie from './data/mock-movie.json';
 
 export const handlers = [
+  // Intercept "GET https://api.themoviedb.org/3/movie/<id-movie>" requests...
+  http.get('https://api.themoviedb.org/3/movie/:id', (req) => {
+    const { id } = req.params;
+    console.log('🚀 ~ http.get ~ id:', id);
+
+    // ...and respond to them using this JSON response.
+    return HttpResponse.json(mockMovie);
+  }),
+
   // Intercept "GET https://api.themoviedb.org/3/movie/popular" requests...
   http.get('https://api.themoviedb.org/3/movie/popular', () => {
     // ...and respond to them using this JSON response.
