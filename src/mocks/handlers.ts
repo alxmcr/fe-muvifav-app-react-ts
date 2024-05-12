04175415b9e6f59c461df166e80bfbea1641bbdf
Alejandro Coca <alxmcr@outlook.com>
2024-05-12 00:51:50 -0400

    wip

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,13 +1,24 @@
 import { http, HttpResponse } from 'msw';
+import mockPopularMovies from './data/mock-popular-movies.json';
+import mockTopRatedMovies from './data/mock-top-rated-movies.json';
+import mockUpcomingMovies from './data/mock-upcoming-movies.json';
 
 export const handlers = [
-  // Intercept "GET https://example.com/user" requests...
-  http.get('https://example.com/user', () => {
+  // Intercept "GET https://api.themoviedb.org/3/movie/popular" requests...
+  http.get('https://api.themoviedb.org/3/movie/popular', () => {
     // ...and respond to them using this JSON response.
-    return HttpResponse.json({
-      id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
-      firstName: 'John',
-      lastName: 'Maverick',
-    });
+    return HttpResponse.json(mockPopularMovies);
+  }),
+
+  // Intercept "GET https://api.themoviedb.org/3/movie/top_rated" requests...
+  http.get('https://api.themoviedb.org/3/movie/top_rated', () => {
+    // ...and respond to them using this JSON response.
+    return HttpResponse.json(mockTopRatedMovies);
+  }),
+
+  // Intercept "GET https://api.themoviedb.org/3/movie/upcoming" requests...
+  http.get('https://api.themoviedb.org/3/movie/upcoming', () => {
+    // ...and respond to them using this JSON response.
+    return HttpResponse.json(mockUpcomingMovies);
   }),
 ];
