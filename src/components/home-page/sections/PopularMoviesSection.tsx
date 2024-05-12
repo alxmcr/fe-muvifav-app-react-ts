db8be9d5330d3572cffb4a4875191a28e78d8776
Alejandro Coca <alxmcr@outlook.com>
2024-05-12 15:24:57 -0400

    wip

--- src/components/home-page/sections/PopularMoviesSection.tsx
+++ src/components/home-page/sections/PopularMoviesSection.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { LoadingStates } from '../../../@types/serviceEnums';
 import usePopularMovies from '../../../hooks/usePopularMovies';
 import GroupCardsMovie from '../../@movies/GroupCardsMovie';
-import Icon44x44ArrowLeft from '../../@icons/44x44/Icon44x44ArrowLeft';
-import Icon44x44ArrowRight from '../../@icons/44x44/Icon44x44ArrowRight';
+import ArrowActionsMovies from '../ArrowActionsMovies';
 
 export default function PopularMoviesSection() {
   const [pageNumber, setPageNumber] = React.useState(1);
@@ -35,17 +34,12 @@ export default function PopularMoviesSection() {
       <div className="flex flex-col gap-4 p-4 md:w-full md:px-10 lg:mx-auto lg:w-[1028px] lg:px-0">
         <header className="flex items-center justify-between">
           <h2 className="font-rowdies text-[2.5rem] font-bold text-golden-300">Popular</h2>
-          <div className="flex items-center justify-between gap-2 text-golden-300">
-            <button onClick={handlePrevPage}>
-              <Icon44x44ArrowLeft />
-            </button>
-            <p className="text-light-50">
-              {pageNumber}/{totalPages}
-            </p>
-            <button onClick={handleNextPage}>
-              <Icon44x44ArrowRight />
-            </button>
-          </div>
+          <ArrowActionsMovies
+            pageNumber={pageNumber}
+            totalPages={totalPages}
+            handlePrevPage={handlePrevPage}
+            handleNextPage={handleNextPage}
+          />
         </header>
         <GroupCardsMovie movies={popularMovies} />
       </div>
