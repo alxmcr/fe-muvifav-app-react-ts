854b1fda9db3cfcc48f44400080a9e68e06178e4
Alejandro Coca <alxmcr@outlook.com>
2024-05-12 19:06:10 -0400

    wip

--- src/components/PosterMovieDetails.tsx
+++ src/components/PosterMovieDetails.tsx
@@ -7,18 +7,30 @@ type Props = {
 
 export default function PosterMovieDetails({ movie, posterSize = 'w500' }: Props) {
   if (movie === null || movie === undefined) {
-    return <img src="/poster-movie.png" alt="Default poster movie" className="h-[250px] w-[156px]" />;
+    return (
+      <img
+        src="/poster-movie.png"
+        alt="Default poster movie"
+        className="hidden h-[250px] w-[156px] md:flex"
+      />
+    );
   }
 
   if (movie.poster_path === null || movie.poster_path === undefined || movie.poster_path === '') {
-    return <img src="/poster-movie.png" alt="Default poster movie" className="h-[250px] w-[156px]" />;
+    return (
+      <img
+        src="/poster-movie.png"
+        alt="Default poster movie"
+        className="hidden h-[250px] w-[156px] md:flex"
+      />
+    );
   }
 
   return (
     <img
       src={`https://image.tmdb.org/t/p/${posterSize}/${movie?.poster_path}`}
       alt={`${movie.title}'s poster`}
-      className="h-[250px] w-[156px] hover:border hover:border-daisy-400"
+      className="hidden h-[250px] w-[156px] hover:border hover:border-daisy-400 md:flex"
     />
   );
 }
