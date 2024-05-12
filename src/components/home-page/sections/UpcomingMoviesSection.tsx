67d69abdd48f50a6f78fc7e435b6aeea33cf1cf7
Alejandro Coca <alxmcr@outlook.com>
2024-05-12 15:50:15 -0400

    wip

---
+++ src/components/home-page/sections/UpcomingMoviesSection.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { LoadingStates } from '../../../@types/serviceEnums';
+import useUpcomingMovies from '../../../hooks/useUpcomingMovies';
+import GroupCardsMovie from '../../@movies/GroupCardsMovie';
+import ArrowActionsMovies from '../ArrowActionsMovies';
+
+export default function UpcomingMoviesSection() {
+  const [pageNumber, setPageNumber] = React.useState(1);
+  const { upcomingMovies, errorUpcomingMovies, statusUpcomingMovies, totalPages } =
+    useUpcomingMovies(pageNumber);
+
+  const handlePrevPage = () => setPageNumber((prevPageNumber) => prevPageNumber - 1);
+  const handleNextPage = () => setPageNumber((prevPageNumber) => prevPageNumber + 1);
+
+  if (LoadingStates.PENDING === statusUpcomingMovies) {
+    return (
+      <section>
+        <h2>Popular</h2>
+        <p>Loading movies...</p>
+      </section>
+    );
+  }
+
+  if (LoadingStates.ERROR === statusUpcomingMovies && errorUpcomingMovies) {
+    return (
+      <section>
+        <h2>Popular</h2>
+        <p>{errorUpcomingMovies.message}</p>
+      </section>
+    );
+  }
+
+  return (
+    <section className="bg-light-950">
+      <div className="flex flex-col gap-4 p-4 md:w-full md:px-10 lg:mx-auto lg:w-[1028px] lg:px-0">
+        <header className="flex items-center justify-between">
+          <h2 className="font-rowdies text-[2.5rem] font-bold text-golden-300">Upcoming</h2>
+          <ArrowActionsMovies
+            pageNumber={pageNumber}
+            totalPages={totalPages}
+            handlePrevPage={handlePrevPage}
+            handleNextPage={handleNextPage}
+          />
+        </header>
+        <GroupCardsMovie movies={upcomingMovies} />
+      </div>
+    </section>
+  );
+}
