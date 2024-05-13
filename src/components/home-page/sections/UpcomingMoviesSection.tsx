a6904fe94a6cd374ec0fe8ccf107936bec564167
Alejandro Coca <alxmcr@outlook.com>
2024-05-13 00:51:04 -0400

    wip

--- src/components/home-page/sections/UpcomingMoviesSection.tsx
+++ src/components/home-page/sections/UpcomingMoviesSection.tsx
@@ -14,25 +14,45 @@ export default function UpcomingMoviesSection() {
 
   if (LoadingStates.PENDING === statusUpcomingMovies) {
     return (
-      <section>
-        <h2>Popular</h2>
-        <p>Loading movies...</p>
+      <section className="bg-light-950">
+        <div className="flex flex-col gap-4 p-4 md:h-[500px] md:w-full md:px-10 lg:mx-auto lg:w-[1028px] lg:px-0">
+          <HeaderSectionMovies
+            title="Upcoming"
+            pageNumber={pageNumber}
+            totalPages={totalPages}
+            handlePrevPage={handlePrevPage}
+            handleNextPage={handleNextPage}
+          />
+          <div className="flex h-full items-center justify-center">
+            <p className="text-golden-300">Loading top rated movies...</p>
+          </div>
+        </div>
       </section>
     );
   }
 
   if (LoadingStates.ERROR === statusUpcomingMovies && errorUpcomingMovies) {
     return (
-      <section>
-        <h2>Popular</h2>
-        <p>{errorUpcomingMovies.message}</p>
+      <section className="bg-light-950">
+        <div className="flex flex-col gap-4 p-4 md:h-[500px] md:w-full md:px-10 lg:mx-auto lg:w-[1028px] lg:px-0">
+          <HeaderSectionMovies
+            title="Upcoming"
+            pageNumber={pageNumber}
+            totalPages={totalPages}
+            handlePrevPage={handlePrevPage}
+            handleNextPage={handleNextPage}
+          />
+          <div className="h-full">
+            <p className="text-golden-300">{errorUpcomingMovies.message}</p>
+          </div>
+        </div>
       </section>
     );
   }
 
   return (
     <section className="bg-light-950">
-      <div className="flex flex-col gap-4 p-4 md:w-full md:px-10 lg:mx-auto lg:w-[1028px] lg:px-0">
+      <div className="flex flex-col gap-4 p-4 md:h-[500px] md:w-full md:px-10 lg:mx-auto lg:w-[1028px] lg:px-0">
         <HeaderSectionMovies
           title="Upcoming"
           pageNumber={pageNumber}
