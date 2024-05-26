a8f9d579804d1243f885466a25552befb6f70c8b
Alejandro Coca <alxmcr@outlook.com>
2024-05-26 19:18:56 -0400

    wip

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,12 +3,9 @@ import MainContent from '../components/home-page/MainContent';
 import PopularMoviesSection from '../components/home-page/sections/PopularMoviesSection';
 import TopRatedMoviesSection from '../components/home-page/sections/TopRatedMoviesSection';
 import UpcomingMoviesSection from '../components/home-page/sections/UpcomingMoviesSection';
-import usePopularMovies from '../hooks/usePopularMovies';
 import HomePageProviders from '../providers/HomePageProviders';
 
 export default function HomePage() {
-  const { popularMovies } = usePopularMovies(1);
-  console.log('🚀 ~ HomePage ~ popularMovies:', popularMovies);
 
   return (
     <HomePageProviders>
